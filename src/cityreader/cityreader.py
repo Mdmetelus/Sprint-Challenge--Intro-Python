8dba38dcb7101298b6528dfde66c97b8e52b8787
Mr. Metelus <Mdmetelus@gmail.com>
2019-03-22 13:59:14 -0400

    City error fixed tested and working  (MVPComplete)

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -39,7 +39,7 @@ def cityreader(cities=[]):
           if line_count ==0:
             line_count += 1
           else:
-            cities.append(city(row[0], float(row[3]), float(row[4])))
+            cities.append(City(row[0], float(row[3]), float(row[4])))
         return cities
 
 cityreader(cities)
