78ba81c3d76f0b9e036802779951655af557681c
Mr. Metelus <Mdmetelus@gmail.com>
2019-03-22 11:36:37 -0400

    base class created and 2  child classes

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,16 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+class Vehicle:
+    def __init__(self):
+        pass
+
+
+class FlightVehicle(Vehicle):
+    def __init__(self):
+        pass
+
+class Starship(FlightVehicle):
+    def __init__(self)
+        pass
